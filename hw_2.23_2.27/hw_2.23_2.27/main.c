4208f1a2aa842ddbf8cd94ce646da4857346bad1
cnfzon <cnfzon.5@gmail.com>
2024-10-03 00:40:21 +0800

    Add files via upload

---
+++ hw_2.23_2.27/hw_2.23_2.27/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("PPPPPPPPP\n    P   P\n    P   P\n    P   P\n     P P \n");
+	printf("\n  JJ     \n J       \nJ        \n J       \n  JJJJJJJ\n\n");
+	printf("DDDDDDDDD\nD       D\nD       D\n D     D \n  DDDDD  \n");
+	return 0;
+}
